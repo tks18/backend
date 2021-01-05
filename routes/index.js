3c3d6ac677582a76bac2cfcce600d5d5bf3ba721
tks18 <tksudharshan@gmail.com>
2021-01-05 20:16:56 +0530

    Vercel Test API

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const { join } = require('path')
+path = process.env.NODE_ENV == "production" ? join(__dirname, "routes") : join(__dirname)
 
 router.get('/', (req, res) => {
   res.send("Super ")
 });
 
-router.use('/author', require('./author'));
+router.use('/author', require(join(path, "author", "index.js")));
 
 module.exports = router;
