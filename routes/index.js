e4e34ee6dbc4e85151f3f3a9c4c14522562f42a6
tks18 <tksudharshan@gmail.com>
2021-02-07 06:23:37 +0530

    Add Route to Check Backend and Database Operational Status

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
+const originCheck = require('../../helpers/checkOrigin');
 const db = require('../helpers/mongo');
 
 router.use('/blog', require('./blog'));
@@ -9,6 +10,26 @@ router.use('/stories', require('./stories'));
 
 router.get(/(\/.*)+/, (req, res) => {
   res.status(401).sendFile(path.resolve(__dirname, '../views/index.html'));
+});
+
+router.post('/', (req, res) => {
+  if (originCheck(req.headers.origin)) {
+    db.connect()
+      .then(() => {
+        res.status(200).json({
+          success: true,
+          message: 'Backend and Database Working',
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ success: false, message: error });
+      });
+  } else {
+    res.status(401).json({
+      success: false,
+      message: 'Forbidden, Wrong way to Communicate',
+    });
+  }
 });
 
 module.exports = router;
