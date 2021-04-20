5f110b428dac87bc0d22893ce2ab134c6a57fcf2
tks18 <tksudharshan@gmail.com>
2021-04-20 19:44:17 +0530

    Security: Allow Requested Headers related to Hash

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader(
     'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type, Accept',
+    'X-Requested-With,content-type, Accept,secure_hash,requested_at',
   );
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
