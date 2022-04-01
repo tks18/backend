e980f9a41736550bde22be790945981f8a2f4ccc
tks18 <tksudharshan@gmail.com>
2022-04-01 19:26:28 +0530

    Introduce Blog Posts using Hashnode API

---
+++ helpers/hashnode.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+
+async function hashnodeQuery(query, variables = {}) {
+  try {
+    const response = await axios.post(
+      'https://api.hashnode.com/',
+      {
+        query,
+        variables,
+      },
+      {
+        Headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    if (response.status === 200) {
+      return {
+        success: true,
+        data: response.data.data,
+        error: null,
+      };
+    }
+    return {
+      success: false,
+      data: null,
+      error: null,
+    };
+  } catch (e) {
+    return {
+      success: false,
+      data: null,
+      error: String(e),
+    };
+  }
+}
+
+module.exports = async function blogQuery() {
+  const queryConstant = `
+    query GetArticles($page: Int!) {
+        user(username: "tks18") {
+            publicationDomain
+            publication {
+                posts(page: $page) {
+                    title
+                    coverImage
+                    slug
+                }
+            }
+        }
+    }
+  `;
+  const data = await hashnodeQuery(queryConstant, { page: 0 });
+  return data;
+};
